458d270e19700ba7c228f2950fef974a7d0246a4
Maksim Klippa <mklippa@ozon.ru>
2019-09-09 22:43:37 +0300

    add components

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>BNW (Price: 110 | Quantity: 10)</h1>
+        <input type="text" placeholder="Quantity">
+        <button>Sell</button>
+    </div>
+</template>
---
+++ src/components/Stock.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>BNW (Price: 110)</h1>
+        <input type="text" placeholder="Quantity">
+        <button>Buy</button>
+    </div>
+</template>
